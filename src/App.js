b9163fc7eec4c738d13ae6ac1b88a7c415fd7ba2
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2024-11-26 09:11:55 -0600

    Se implemento el slice de las tareas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { useSelector } from 'react-redux';
 
 function App() {
+
+  const tasksState = useSelector(state => state.tasks)
+  console.log(tasksState)
+  
   return (
     <div className="App">
       <h1>Hello word</h1>
