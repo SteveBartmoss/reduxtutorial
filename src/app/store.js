7dacf29446d820f5bc56eb271d788561b5a7c18c
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2024-11-26 09:11:32 -0600

    Se implemento el slice en el estado global

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,8 @@
 import {configureStore} from '@reduxjs/toolkit'
+import tasksReducer from '../features/task/taskSlice'
 
+// almacenamiento vacio global
 export const store = configureStore({
-
+    task: tasksReducer
 })
+
