627aaa35f5cc0d1362d163c61c54568d91a45ccc
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2024-11-27 09:03:17 -0600

    Se removio la implementacion del store ya q

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import './App.css';
 import { useSelector } from 'react-redux';
 
 function App() {
-
-  const tasksState = useSelector(state => state.tasks)
-  console.log(tasksState)
   
   return (
     <div className="App">
