89e6de429b2e395177d1142e442bb945a22abf86
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-11-30 10:08:47 -0600

    Se agrego la implementacion de los componentes para el formulario de tareas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import { useSelector } from 'react-redux';
+import { TaskForm } from './components/TaskForm';
+import { TasksList } from './components/TasksList';
 
 function App() {
   
   return (
     <div className="App">
       <h1>Hello word</h1>
+      <TaskForm />
+      <TasksList />
     </div>
   );
 }
